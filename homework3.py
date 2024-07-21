38ed7bf8f84684d3635e0f260c0039b951cdaeb7
Palyasolo <soloduhin.pavel@ya.ru>
2024-07-21 13:28:47 +0700

    Delete homework3.py

--- homework3.py
+++
@@ -1,8 +0,0 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-N = len(my_list)
-index = 0
-while index < N:
-    if my_list[index]<0:
-        print (my_list[index])
-        break
-    index += 1
